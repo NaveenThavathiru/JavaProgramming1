3051c1bc1e7e58fc8cd78691f735a74d26a011c1
NaveenThavathiru <Nt0981658@RK03-B1225-06.francistuttle.edu>
2018-01-26 10:30:50 -0600

    Added Chapter 8

--- src/Chapter7/C7_26.java
+++ src/Chapter7/C7_26.java
@@ -1,49 +1,56 @@
-
 package Chapter7;
 
 import java.util.Scanner;
+
 /**
- * 
+ * Program check if the two lists entered by the user is identical or not
+ *
  * @author Naveen Thavathiru
  */
 public class C7_26 {
+
     /**
      * Main Method
      *
      * @param args arguments from the command line
      */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-		// Tells the user to enter two lists
-		System.out.print("Enter list1: ");
-		int[] list1 = new int[input.nextInt()];
-		for (int i = 0; i < list1.length; i++)
-			list1[i] = input.nextInt(); 	
+        // Tells the user to enter two lists
+        System.out.print("Enter list1: ");
+        int[] list1 = new int[input.nextInt()];
+        for (int i = 0; i < list1.length; i++) {
+            list1[i] = input.nextInt();
+        }
 
-		System.out.print("Enter list2: ");
-		int[] list2 = new int[input.nextInt()];
-		for (int i = 0; i < list2.length; i++)
-			list2[i] = input.nextInt();
+        System.out.print("Enter list2: ");
+        int[] list2 = new int[input.nextInt()];
+        for (int i = 0; i < list2.length; i++) {
+            list2[i] = input.nextInt();
+        }
 
-		// Display whether the two are strictly identical
-		System.out.println("Two lists are" + (equals(list1, list2) ? " " : " not ") + "strictly identical");
-	}
-        /**
-         * 
-         * @param list1 The first list entered by the user
-         * @param list2 The second list entered by the user
-         * @return True of false, based on if they are identical or not
-         */
-	public static boolean equals(int[] list1, int[] list2) {
-		if (list1.length != list2.length)
-			return false;
+        // Display whether the two are strictly identical
+        System.out.println("Two lists are" + (equals(list1, list2) ? " " : " not ") + "strictly identical");
+    }
 
-		for (int i = 0; i < list1.length; i++) {
-			if (list1[i] != list2[i])
-				return false;
-		}
-		return true;
-	}
+    /**
+     * Method to check if 2 arrays are equal
+     *
+     * @param list1 The first list entered by the user
+     * @param list2 The second list entered by the user
+     * @return True of false, based on if they are identical or not
+     */
+    public static boolean equals(int[] list1, int[] list2) {
+        if (list1.length != list2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
-
--- src/Chapter7/P7.java
+++ src/Chapter7/P7.java
@@ -1,50 +1,56 @@
-
 package Chapter7;
+
 import java.util.Scanner;
+
 /**
  * Program determines if both lists are strictly identical or not
- * 
+ *
  * @author Naveen Thavathiru
  */
 public class P7 {
+
     /**
      * Main Method
-     * 
+     *
      * @param args arguments from command line prompt
      */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-		// Tells the user to enter two lists
-		System.out.print("Enter list1: ");
-		int[] list1 = new int[input.nextInt()];
-		for (int i = 0; i < list1.length; i++)
-			list1[i] = input.nextInt(); 	
+        // Tells the user to enter two lists
+        System.out.print("Enter list1: ");
+        int[] list1 = new int[input.nextInt()];
+        for (int i = 0; i < list1.length; i++) {
+            list1[i] = input.nextInt();
+        }
 
-		System.out.print("Enter list2: ");
-		int[] list2 = new int[input.nextInt()];
-		for (int i = 0; i < list2.length; i++)
-			list2[i] = input.nextInt();
+        System.out.print("Enter list2: ");
+        int[] list2 = new int[input.nextInt()];
+        for (int i = 0; i < list2.length; i++) {
+            list2[i] = input.nextInt();
+        }
 
-		// Display whether the two are strictly identical
-		System.out.println("Two lists are" + (equals(list1, list2) ? " " : " not ") + "strictly identical");
-	}
-        /**
-         * 
-         * @param list1 1st list entered by user    
-         * @param list2 2nd list entered by user
-         * @return returns either true of false (based on if the lists are similar or not.)
-         */
-	public static boolean equals(int[] list1, int[] list2) {
-		if (list1.length != list2.length)
-			return false;
+        // Display whether the two are strictly identical
+        System.out.println("Two lists are" + (equals(list1, list2) ? " " : " not ") + "strictly identical");
+    }
 
-		for (int i = 0; i < list1.length; i++) {
-			if (list1[i] != list2[i])
-				return false;
-		}
-		return true;
-	}
+    /**
+     *
+     * @param list1 1st list entered by user
+     * @param list2 2nd list entered by user
+     * @return returns either true of false (based on if the lists are similar
+     * or not.)
+     */
+    public static boolean equals(int[] list1, int[] list2) {
+        if (list1.length != list2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
-
-
